da4b2ab97b21e7b8c3eb2eec47271357e05201c5
rabindra <rabindrasapkota2@gmail.com>
2023-06-05 12:52:57 +0545

    Initialized

---
+++ js/3_variables.js
@@ -0,0 +1,15 @@
+var num_var = 123
+var float_var = 3.14
+var string_var = "Rabindra"
+var undefined_var
+var array_var = [1, "Rabindra"]
+var object_var = {name:"Rabindra", age:27}
+var mixed_var = [1, "rabi", ["JS", 5], {home:"Narephat", house:23}]
+
+document.write(num_var+" <br/>")
+document.write(float_var+" <br/>")
+document.write(string_var+" <br/>")
+document.write(undefined_var+" <br/>")
+document.write(array_var+" <br/>")
+document.write(object_var+" <br/>")
+document.write(mixed_var+" <br/>")
