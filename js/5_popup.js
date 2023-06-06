227979fe57f05ed234c9e0e27e06a7663341eec1
rabindra <rabindrasapkota2@gmail.com>
2023-06-06 10:32:16 +0545

    Popup Notes

---
+++ js/5_popup.js
@@ -0,0 +1,10 @@
+alert("This is alert Popup") 
+// Shows alert message in browser for notification. One ok button only
+
+confirm("Do you want to proceed")
+// Shows confirm message similar to alert. Two buttons cancel and okay
+
+var user_name = prompt("Enter Your Name")
+document.write("Hello "+user_name+"<br/>")
+// Pop-up for gathering data from user. Two buttons cancel and okay
+// In example: user_name is gathered from user and message is written in web-page as Hello {name}
