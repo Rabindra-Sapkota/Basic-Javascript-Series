1a30d77e31671e08f5c43267c73169110888371f
rabindra <rabindrasapkota2@gmail.com>
2023-06-06 12:23:40 +0545

    Reference Materials

---
+++ js/16_inbuilt_function.js
@@ -0,0 +1,5 @@
+// Reference: https://www.tutorialspoint.com/javascript/javascript_builtin_functions.htm
+
+
+// For Event Detail:
+// https://www.tutorialspoint.com/javascript/javascript_events.htm
