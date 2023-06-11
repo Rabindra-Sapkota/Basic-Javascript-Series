c0a43602e173ce9aef61b4d7afae3eb3e28c7411
rabindra <rabindrasapkota2@gmail.com>
2023-06-11 14:43:45 +0545

    Notes on BOM added

--- js/20_DOM.js
+++ js/20_DOM.js
@@ -7,3 +7,19 @@ function change_attribute_after_load(){
     document.getElementById("para_1").style.color = "blue";
     document.getElementById("para_1").style.fontSize = "xx-large";
 }
+
+
+/*
+Similar to DOM BOM (Browser Object model gives access to browser properties)
+
+Examples:
+    document.write(window.screen.width + " " + window.screen.height)
+    window.history.forward()    // Go one step forward in browser history
+    window.location = "www.google.com"
+    navigator.appName
+    navigator.appCodeName
+    navigator.cookieEnabled
+    for (x in navigator){
+        document.write(x+"<br/>")
+    }
+*/
